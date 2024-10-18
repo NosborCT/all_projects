ecf6f9ff571f72346e963028ca29c3ad78eac2f5
NosborCT <hainiken08@gmail.com>
2024-10-18 10:16:25 -0300

    fix: correcao no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#123331] flex flex-col items-center justify-center min-h-screen p-4`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#123331] flex flex-col items-center justify-center p-7`}
       >
         <Header/>
         {children}
