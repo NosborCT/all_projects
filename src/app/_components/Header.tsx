1e1fe3b389f54afdab816f3ad034119a18419f44
NosborCT <hainiken08@gmail.com>
2024-10-18 16:08:43 -0300

    feat: Adicionado o componente sheet ao botao de projetos

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,6 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+import {
+  Sheet,
+  SheetTrigger,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetDescription,
+  SheetClose,
+} from "@/components/ui/sheet";
+
 const Header = () => {
   return (
     <>
@@ -10,7 +20,30 @@ const Header = () => {
               <h1 className=" text-white text-2xl font-bold">Home</h1>
             </div>
           </Link>
-          <Button className=" transition duration-300 bg-[#FC7F42] hover:bg-[#D9D9D9] hover:text-black text-lg ">Projetos</Button>
+
+
+          <Sheet>
+          <SheetTrigger asChild>
+            <Button className=" transition duration-300 bg-[#FC7F42] hover:bg-[#D9D9D9] hover:text-black text-lg ">Projetos</Button>
+
+
+          </SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[540px] bg-[#053D39] ">
+            <SheetHeader>
+              <SheetTitle className=" text-white mb-5" >Projetos:</SheetTitle>
+              <SheetDescription className=" text-white">
+                  
+                  <Link href = "/pages/bin2dec">
+                    
+                    <SheetClose asChild>
+                      <Button className=" flex w-full max-w-md  font-semibold transition duration-300 bg-[#FC7F42] hover:bg-[#D9D9D9] hover:text-black" >Bin2Dec</Button>
+                    </SheetClose>
+                  </Link>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+
       </header>
     </>
   );
