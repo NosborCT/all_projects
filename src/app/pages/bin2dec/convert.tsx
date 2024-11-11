4701576ccd98cf8c0a692678edff3613c86db259
NosborCT <hainiken08@gmail.com>
2024-11-11 16:39:32 -0300

    Refactor: Refatorado o bin2dec

--- src/app/pages/bin2dec/convert.tsx
+++ src/app/pages/bin2dec/convert.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-
+import { Card, CardContent } from "@/components/ui/card";
 
 const Bin2Dec = () => {
   const [binaryText, setBinaryText] = useState("");
@@ -18,10 +18,7 @@ const Bin2Dec = () => {
 
     setErrorMessage(""); // Reset na mensagem de erro
 
-    const reversedBinaryText = binaryText
-      .split("")
-      .map(Number)
-      .reverse();
+    const reversedBinaryText = binaryText.split("").map(Number).reverse();
 
     const result = reversedBinaryText.reduce(
       (accumulator, currentValue, idx) =>
@@ -33,46 +30,51 @@ const Bin2Dec = () => {
   };
 
   return (
-    <main className="max-w-3xl w-full p-7 bg-[#053D39]">
-      <div className="flex flex-col items-center">
-        <h1 className="text-[#f7f7f7] text-3xl">
-          Conversão de Binário para Decimal
-        </h1>
-      </div>
+    <div className="min-h-screen bg-gradient-to-b from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800">
+      <main>
+        <Card className="col-span-1 md:col-span-2 mt-10">
+          <CardContent>
+            <div className="flex flex-col items-center">
+              <h1 className="text-3xl p-7 font-bold  text-center text-gray-900 dark:text-white">
+                Conversão de Binário para Decimal
+              </h1>
+            </div>
 
-      <div className="flex text-[#f7f7f7] flex-col items-center mt-10">
-        <div className="flex items-center w-full max-w-md space-x-2">
-          <Input
-            type="text"
-            id="binaryInput"
-            placeholder="Digite o número binário"
-            value={binaryText}
-            onChange={(e) => setBinaryText(e.target.value)}
-          />
-          <Button
-            onClick={convert}
-            className="transition duration-300 bg-[#FC7F42] hover:bg-[#D9D9D9] hover:text-black text-lg"
-            type="button"
-          >
-            Converter
-          </Button>
-        </div>
+            <div className="flex flex-col items-center font-semibold text-gray-900 dark:text-white">
+              <div className="flex items-center w-full max-w-md space-x-2">
+                <Input
+                  type="text"
+                  id="binaryInput"
+                  placeholder="Digite o número binário"
+                  value={binaryText}
+                  onChange={(e) => setBinaryText(e.target.value)}
+                />
+                <Button
+                  onClick={convert}
+                  type="button"
+                >
+                  Converter
+                </Button>
+              </div>
 
-        {errorMessage && (
-          <span className="text-red-500 mt-2">{errorMessage}</span>
-        )}
+              {errorMessage && (
+                <span className="text-red-500 mt-2">{errorMessage}</span>
+              )}
 
-        <div className="max-w-md w-full mt-5">
-          <Input
-            type="text"
-            placeholder="Resultado"
-            value={decimalText}
-            readOnly
-            className="w-full"
-          />
-        </div>
-      </div>
-    </main>
+              <div className="max-w-md w-full mt-5">
+                <Input
+                  type="text"
+                  placeholder="Resultado"
+                  value={decimalText}
+                  readOnly
+                  className="w-full"
+                />
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </main>
+    </div>
   );
 };
 
